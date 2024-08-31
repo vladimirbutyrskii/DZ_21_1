773df587db8445ed21c6fb80dab82ae617abac17
vladimirbutyrskii <vlaver@mail.ru>
2024-08-31 20:56:06 +0300

    slug commit

--- myblog/models.py
+++ myblog/models.py
@@ -6,7 +6,7 @@ class Myblog(models.Model):
         help_text="Введите заголовок",
     )
 
-    slug = models.CharField(max_length=150, verbose_name="Slug",)
+    slug = models.CharField(max_length=150, verbose_name="Slug", null=True, blank=True)
     body = models.TextField(verbose_name="Содержимое", help_text="Введите текст поста")
     preview = models.ImageField (
         upload_to="myblog/photo",
